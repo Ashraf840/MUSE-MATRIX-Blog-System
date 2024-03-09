3ba282f6c6c2d1a72d7b57708de39b643ca22a5c
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-10 00:34:21 +0600

    Added Latest Posts section, where the dummy posts are generated dunamically using JS

---
+++ build/js/iterative-html-elements/latest-post-items-chunk.js
@@ -0,0 +1,29 @@
+let latest_post_items_chunk = `
+<div class="item">
+<div class="images">
+    <img src="../assets/images/hero/hero-slider-img-1.jpg" alt="latest-post-pic"
+        class="rounded latest-post-pic" />
+</div>
+<div class="flex justifycenter flex-row py-4">
+    <a href="#" class="text-orange-600 hover:text-orange-800">Business, Travel</a>
+    <a href="#" class="text-orange-800 hover:text-orange-600">&nbsp; - March 7, 2024</a>
+</div>
+<div class="title">
+    <a href="#" class="text-2xl font-medium text-gray-800 hover:text-gray-600">Your most unhappy
+        customers are your
+        greatest source of leaning</a>
+</div>
+<p class="text-gray-500 py-3">
+    Even the all-powerful Pointing has no control about the blind texts it is an almost
+    unorthographic life One dat however a small line of blind text by the name of Lorem
+    Ipsum decided to leave for the far WoWorl of Grammar.
+</p>
+<div class="author flex py-5">
+    <img src="../assets/images/hero/author.jpg" alt="author-pic-1" class="author-pic rounded-full" />
+    <div class="flex flex-col justify-center px-4">
+        <a class="text-md font-medium text-gray-800 hover:text-gray-600">Flying High</a>
+        <span class="text-xs text-gray-500">CEO and Founder</span>
+    </div>
+</div>
+</div>
+`;
