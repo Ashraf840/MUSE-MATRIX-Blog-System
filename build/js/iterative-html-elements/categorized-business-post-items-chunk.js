5bc693dc9f2f2fcd4cc30bed7410c05084fc3030
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-13 23:40:46 +0600

    Created the categorized posts section (business & travel); Also made the section responsive

---
+++ build/js/iterative-html-elements/categorized-business-post-items-chunk.js
@@ -0,0 +1,27 @@
+let categorized_business_post_items_chunk = `
+<div class="flex gap-5 categorizedBusinessPostsItem">
+                            <div class="image flex flex-col justify-start">
+                                <img src="../assets/images/hero/hero-slider-img-1.jpg" alt="categorized-post-pic"
+                                    class="rounded categorized-post-pic" />
+                            </div>
+                            <div class="info flex flex-col justify-center">
+                                <div class="cat">
+                                    <a href="#" class="text-orange-600 hover:text-orange-800">Business, Travel</a>
+                                    <a href="#" class="text-orange-800 hover:text-orange-600">- March 7, 2024</a>
+                                </div>
+                                <div class="title">
+                                    <a href="#" class="title-anchor">Your most unhappy customers are your
+                                        greatest source of learning</a>
+                                </div>
+                                <div class="author flex py-5">
+                                    <img src="../assets/images/hero/author.jpg" alt="author-pic-1"
+                                        class="author-pic rounded-full" />
+                                    <div class="flex flex-col justify-center px-4">
+                                        <a class="author-title text-md font-medium text-gray-800 hover:text-gray-600">Flying
+                                            High</a>
+                                        <span class="text-xs text-gray-500">CEO and Founder</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+`;
