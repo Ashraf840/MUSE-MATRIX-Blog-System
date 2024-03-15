0b2ab58ce21c6ddf41104e6063bf9159fbbe9a88
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-15 13:04:50 +0600

    Created the related-post section & made that responsive

---
+++ build/js/iterative-html-elements/single-blog-page/related-post-items-chunk.js
@@ -0,0 +1,27 @@
+let related_post_items_chunk = `
+<div class="relatedPostBodyFlex">
+                    <div class="image flex flex-col justify-start">
+                        <img src="../assets/images/hero/hero-slider-img-1.jpg" alt="related-post-pic"
+                            class="rounded related-post-pic" />
+                    </div>
+                    <div class="info">
+                        <div class="cat">
+                            <a href="#" class="text-orange-600 hover:text-orange-800">Business, Travel</a>
+                            <a href="#" class="text-orange-800 hover:text-orange-600">- March 7, 2024</a>
+                        </div>
+                        <div class="title">
+                            <a href="#" class="title-anchor">Your most unhappy customers are your
+                                greatest source of learning</a>
+                        </div>
+                        <div class="author">
+                            <img src="../assets/images/hero/author.jpg" alt="author-pic-1"
+                                class="author-pic rounded-full" />
+                            <div class="authorFlex">
+                                <a class="author-title text-md font-medium text-gray-800 hover:text-gray-600">Flying
+                                    High</a>
+                                <span class="text-xs text-gray-500">CEO and Founder</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+`;
