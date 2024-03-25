cdd9ee63a96640c78b9ef1c92ea5c29727022c34
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-26 00:06:11 +0600

    Send email form data to the backend

--- newsletter/views.py
+++ newsletter/views.py
@@ -6,12 +6,33 @@ from django.shortcuts import render
 
 class NewsletterPage(View):
 
+    template_name = 'newsletter/newsletterPage.html'
+
+    def get_subscribed_emails(self):
+        return Newsletter.objects.all()
+
     def get(self, request):
-        subscribed_emails = Newsletter.objects.all()
+        subscribed_emails = self.get_subscribed_emails()
         context = {
             'subscribed_emails': subscribed_emails
         }
-        return render(request, 'newsletter/newsletterPage.html', context=context)
+        return render(request, self.template_name , context=context)
+    
+    def post(self, request):
+        # Handle post-data
+        email_addresses=request.POST.get('recipient')
+        if email_addresses:
+            email_list = [email.strip() for email in email_addresses.split(',')] # split on comma & then strip the leading & trailing whitespaces
+        print("email_list:", email_list)
+        subject=request.POST.get('subject')
+        message=request.POST.get('email-body')
+        # Send email
+        # Render the newsletter page
+        subscribed_emails = self.get_subscribed_emails()
+        context = {
+            'subscribed_emails': subscribed_emails
+        }
+        return render(request, self.template_name , context=context)
 
 class newsletterSubscription(View):
 
